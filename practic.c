94af44f8c01a46caed58420d9ec607977bfed0c5
Keshav Sharma <sharmakeshav802@gmail.com>
2025-10-28 18:27:30 +0530

    changes for op

---
+++ practic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    // int a=5,b=10;
+    // printf("a-b= %d\n",a-b);
+    int a,b;
+    printf("Enter two values");
+
+    scanf("%d %d",&a,&b);
+    printf("a+b= %d\n",a+b);
+    printf("a-b= %d\n",a-b);
+    printf("a*b= %d\n",a*b);
+    printf("a/b= %d\n",a/b);
+    return 0;
+}
---
+++ sanisha.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+int main()
+{
+    /*
+    taking 2 input and then  returning sum
+    int a,b,sum;
+    printf("enter two number");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("%d",sum);
+    return 0; 
+    
+    */
+    
+    //  int a=5;
+    //  float b=12.5;
+    //  char ch='*';
+    //  printf("%d %.2f %c",a,b,ch);
+
+    //
+    printf("my name is sanisha\tch \n");
+    printf("my name is sanisha\\ch \n");
+    printf("and that's how we write \"c lenguage\" :\n");  
+
+
+    // Oprators     
+    // Arithmetic Operators 
+
+    // int a=5,b=10;
+    // printf("a+b= %d\n",a+b);
+    // printf("a-b= %d\n",a-b);
+    // printf("a*b= %d\n",a*b);
+    // printf("a/b= %d\n",b/a);
+    // printf("a%%b= %d\n",b%a);
+
+    //  Relational Operators
+    // int a=5,b=10;
+    // printf("a==b = %d\n",a==b);
+    // printf("a!=b= %d\n",a!=b);
+    // printf("a>b= %d\n",a>b);
+    // printf("a<b= %d\n",a<b);
+    // printf("a>=b= %d\n",a>=b);
+    // printf("a<=b= %d\n",a<=b);
+
+    // Logical Operators 
+
+    // int a=5,b=10;
+    // printf("AND OP: %d\n",(a>0) && (b<0));
+    // printf("or OP: %d\n",(a>0) || (b<0)); 
+    // printf("not OP: %d\n",!(a>0)); 
+return 0;
+}
+
